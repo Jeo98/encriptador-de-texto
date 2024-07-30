7b77ca7973cf2f9bd42beb9cfff3013371752de7
unknown <jeo98dev@gmail.com>
2024-07-30 12:47:23 -0300

    lo mismo que anterior commit, solo que datos guardados

--- app.js
+++ app.js
@@ -28,10 +28,29 @@ for( i=0; i<(texto_usuario.length); i++){
         // al final del arreglo(length)        
   i++;//incremento indice del arreglo para recorrerlo completamente
  }//fin For
+ return arreglo_aux;
 
 }
 
 function desencriptarTexto(texto_usuario){
 
+    //el razonamiento es el mismo a la funcion encriptarTexto, solo que se debe encontrar la palabra clave para remplazar
+    //por la vocal correspondiente
+
+    let texto_desencriptado=texto_usuario;
+
+    texto_desencriptado= texto_desencriptado.replace(/ai/g,'a');
+    texto_desencriptado=texto_desencriptado.replace(/enter/g,'e');
+    texto_desencriptado=texto_desencriptado.replace(/imes/g,'i');
+    texto_desencriptado=texto_desencriptado.replace(/ober/g,'o');
+    texto_desencriptado=texto_desencriptado.replace(/ufat/g,'u');
+    //replace buscará y remplazará todas las OCURRENCIAS( cuando encuetra remplaza)
+
+    return texto_desencriptado;
+}
+
+function CopiarTexto(){
+
     
+
 }
